8da50fe1e475ba87c5a2335cc94400d736dfb371
michelle <michelle.dorothy.dialogo@gmail.com>
2026-03-30 12:56:05 +0800

    added some functions def, still not done

--- converted.py
+++ converted.py
@@ -9,10 +9,25 @@ class MainUI(QMainWindow):
         self.ui = uic.loadUi("KioskProject.ui")
 
         self.ui.pages.setCurrentWidget(self.ui.page1)
+        self.ui.gawaKaDitoNgButtonOrAnyClickingObject.clicked.connect(self.showPage1)
     
     def show(self):
         self.ui.show()
 
+    def showPage1(self):
+        self.ui.pages.setCurrentWidget(self.ui.objectNameTohTeh)
+
+    def showPage2():
+        pass
+
+    def showPage3():
+        pass
+
+    def showPage4():
+        pass
+
+        #so, sa ngayon, ayusin mo muna yung design.
+ 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     burger = MainUI()
